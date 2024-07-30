12ef72e70cb6e9f1179cfb7eda2da938ee289499
fysherman <duykhanh29091999@gmail.com>
2024-07-30 21:52:44 +0700

    chore: deploy

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,12 +1,9 @@
 'use client';
-import { OurFileRouter } from '@/app/api/uploadthing/core';
-import { UploadDropzone } from '@uploadthing/react';
 import { Trash } from 'lucide-react';
 import Image from 'next/image';
 import { UploadFileResponse } from 'uploadthing/client';
 import { IMG_MAX_LIMIT } from './forms/product-form';
 import { Button } from './ui/button';
-import { useToast } from './ui/use-toast';
 
 interface ImageUploadProps {
   onChange?: any;
@@ -14,19 +11,11 @@ interface ImageUploadProps {
   value: UploadFileResponse[];
 }
 
-export default function FileUpload({
-  onChange,
-  onRemove,
-  value
-}: ImageUploadProps) {
-  const { toast } = useToast();
+export default function FileUpload({ onRemove, value }: ImageUploadProps) {
   const onDeleteFile = (key: string) => {
     const files = value;
     let filteredFiles = files.filter((item) => item.key !== key);
     onRemove(filteredFiles);
-  };
-  const onUpdateFile = (newFiles: UploadFileResponse[]) => {
-    onChange([...value, ...newFiles]);
   };
   return (
     <div>
@@ -58,44 +47,7 @@ export default function FileUpload({
             </div>
           ))}
       </div>
-      <div>
-        {value.length < IMG_MAX_LIMIT && (
-          <UploadDropzone<OurFileRouter>
-            className="ut-label:text-sm ut-allowed-content:ut-uploading:text-red-300 py-2 dark:bg-zinc-800"
-            endpoint="imageUploader"
-            config={{ mode: 'auto' }}
-            content={{
-              allowedContent({ isUploading }) {
-                if (isUploading)
-                  return (
-                    <>
-                      <p className="mt-2 animate-pulse text-sm text-slate-400">
-                        Img Uploading...
-                      </p>
-                    </>
-                  );
-              }
-            }}
-            onClientUploadComplete={(res) => {
-              // Do something with the response
-              const data: UploadFileResponse[] | undefined = res;
-              if (data) {
-                onUpdateFile(data);
-              }
-            }}
-            onUploadError={(error: Error) => {
-              toast({
-                title: 'Error',
-                variant: 'destructive',
-                description: error.message
-              });
-            }}
-            onUploadBegin={() => {
-              // Do something once upload begins
-            }}
-          />
-        )}
-      </div>
+      <div>{value.length < IMG_MAX_LIMIT && <></>}</div>
     </div>
   );
 }
