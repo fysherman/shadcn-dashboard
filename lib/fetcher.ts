c01c04de755bdcdba06f756afbd50e1a9438d6b0
KhanhBD4 <KhanhBD4@fpt.com>
2024-08-05 17:49:08 +0700

    chore: fetch

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -8,6 +8,9 @@ export interface FetcherParams {
   headers?: Record<string, any>;
   method?: 'POST' | 'GET' | 'PUT' | 'DELETE';
   withToken?: boolean;
+  onError?: (error: any) => void;
+  onSuccess?: (data: any) => void;
+  onSettled?: () => void;
 }
 
 // const BASE_API = 'https://phandc-fci-pc.tail58805.ts.net';
@@ -15,8 +18,8 @@ const BASE_API = 'http://10.36.7.9:8000';
 
 export default function useFetcher(initParams: Readonly<FetcherParams>) {
   const [loading, setLoading] = useState<boolean>(false);
-  const [data, setData] = useState<any>();
-  const [error, setError] = useState<any>();
+  const [data, setData] = useState<any>(null);
+  const [error, setError] = useState<any>(null);
 
   async function fetchData(triggerParams: Readonly<FetcherParams>) {
     setLoading(true);
@@ -30,11 +33,17 @@ export default function useFetcher(initParams: Readonly<FetcherParams>) {
       params,
       headers,
       rawBody,
-      withToken
+      withToken,
+      onError = () => {},
+      onSuccess = () => {},
+      onSettled = () => {}
     } = {
       ...initParams,
       ...triggerParams
     };
+    const computedUrl = params
+      ? `${BASE_API}${url}?${new URLSearchParams(params).toString()}`
+      : `${BASE_API}${url}`;
     const computedHeaders = {
       'Content-Type': 'application/json',
       ...(withToken && {
@@ -50,23 +59,21 @@ export default function useFetcher(initParams: Readonly<FetcherParams>) {
     };
 
     try {
-      const response = await fetch(
-        params
-          ? `${BASE_API}${url}?${new URLSearchParams(params).toString()}`
-          : `${BASE_API}${url}`,
-        resources
-      );
+      const response = await fetch(computedUrl, resources);
 
       if (!response.ok) throw new Error(response.status.toString());
 
       const data = await response.json();
-      console.info(url, data);
 
       setData(data);
+      onSuccess(data);
+      console.info(url, data);
     } catch (error: any) {
-      console.warn(url, error);
       setError(error);
+      onError(error);
+      console.warn(url, error);
     } finally {
+      onSettled();
       setLoading(false);
     }
   }
